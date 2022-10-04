a2c68d3af1c595fba01192026c62648a49b360b2
PriscilaLSouza <priscila.souza@cmosdrake.com.br>
2022-10-04 15:42:54 -0300

    feature controle de usuarios

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,6 +1,7 @@
-import { Box, Button, Flex, Heading, Icon } from "@chakra-ui/react";
-import { RiAddLine } from "react-icons/ri";
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr } from "@chakra-ui/react";
+import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
+import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
@@ -15,9 +16,69 @@ export default function UserList() {
                     <Flex mb="8" justify="space-between" align="center">
                         <Heading size="lg" fontWeight="normal">Usuários</Heading>
 
-                        <Button as="a" size="sm" fontSize="sm" colorScheme="pink" leftIcon={<Icon as={RiAddLine} />}>Criar novo</Button>
+                        <Button as="a" size="sm" fontSize="sm" colorScheme="pink" leftIcon={<Icon as={RiAddLine} fontSize="20" />}>Criar novo</Button>
                     </Flex>
 
+                    <Table colorScheme="whiteAlpha">
+                        <Thead>
+                            <Tr>
+                                <Th px="6" color="gray.300" width="8">
+                                    <Checkbox colorScheme="pink" />
+                                </Th>
+                                <Th>Usuário</Th>
+                                <Th>Data de cadastro</Th>
+                                <Th width="8"></Th>
+                            </Tr>
+                        </Thead>
+                        <Tbody>
+                            <Tr>
+                                <Td px="6">
+                                    <Checkbox colorScheme="pink" />
+                                </Td>
+                                <Td>
+                                    <Box>
+                                        <Text fontWeight="bold">Priscila Souza</Text>
+                                        <Text fontSize="sm" color="gray.300">priscila_soares.souza@hotmail.com</Text>
+                                    </Box>
+                                </Td>
+                                <Td>04 de Outubro, 2022</Td>
+                                <Td>
+                                <Button as="a" size="sm" fontSize="sm" colorScheme="purple" leftIcon={<Icon as={RiPencilLine} fontSize="16" />}>Editar</Button>
+                                </Td>
+                            </Tr>
+                            <Tr>
+                                <Td px="6">
+                                    <Checkbox colorScheme="pink" />
+                                </Td>
+                                <Td>
+                                    <Box>
+                                        <Text fontWeight="bold">Priscila Souza</Text>
+                                        <Text fontSize="sm" color="gray.300">priscila_soares.souza@hotmail.com</Text>
+                                    </Box>
+                                </Td>
+                                <Td>04 de Outubro, 2022</Td>
+                                <Td>
+                                <Button as="a" size="sm" fontSize="sm" colorScheme="purple" leftIcon={<Icon as={RiPencilLine} fontSize="16" />}>Editar</Button>
+                                </Td>
+                            </Tr>
+                            <Tr>
+                                <Td px="6">
+                                    <Checkbox colorScheme="pink" />
+                                </Td>
+                                <Td>
+                                    <Box>
+                                        <Text fontWeight="bold">Priscila Souza</Text>
+                                        <Text fontSize="sm" color="gray.300">priscila_soares.souza@hotmail.com</Text>
+                                    </Box>
+                                </Td>
+                                <Td>04 de Outubro, 2022</Td>
+                                <Td>
+                                <Button as="a" size="sm" fontSize="sm" colorScheme="purple" leftIcon={<Icon as={RiPencilLine} fontSize="16" />}>Editar</Button>
+                                </Td>
+                            </Tr>
+                        </Tbody>
+                    </Table>
+                        <Pagination />
                 </Box>
             </Flex>
         </Box>
