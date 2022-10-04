d37c4c5b1351a9e881127bf4a296a8aadcc47ebb
PriscilaLSouza <priscila.souza@cmosdrake.com.br>
2022-10-04 16:59:18 -0300

    feature Header responsivo

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-            <Text>Priscila Souza</Text>
-            <Text color="gray.300" fontSize="small">
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Priscila Souza</Text>
+          <Text color="gray.300" fontSize="small">
             priscila_soares.souza@hotmail.com
-            </Text>
-      </Box>
+          </Text>
+        </Box>
+      )}
       <Avatar
         size="md"
         name="Priscila Souza"
