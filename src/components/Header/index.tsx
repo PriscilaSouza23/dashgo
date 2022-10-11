b533ae2ee3fe22a6e8878eebd77119d41617dc3f
PriscilaLSouza <priscila.souza@cmosdrake.com.br>
2022-10-11 16:33:09 -0300

    feature sidebarResponsivo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,18 @@
-import { Flex, useBreakpointValue } from "@chakra-ui/react";
+import { Flex, Icon, IconButton, useBreakpointValue } from "@chakra-ui/react";
+import { RiMenuLine } from "react-icons/ri";
+import { useSidebarDrawer } from "../../contexts/SidebarDrawerContext";
 import { Logo } from "./Logo";
 import { NotificationsNav } from "./NotificationsNav";
 import { Profile } from "./Profile";
 import { SearchBox } from "./SearchBox";
 
-export function Header() {
+export function Header() {  
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
   })
+
+  const { onOpen } = useSidebarDrawer()
 
   return (
     <Flex
@@ -21,6 +25,19 @@ export function Header() {
       px="6"
       align="center"
     >
+      { !isWideVersion && (
+        <IconButton
+          aria-label="Open Drawer navigation"
+          icon={<Icon as={RiMenuLine} />}
+          fontSize="24"
+          variant="unstyled"
+          onClick={onOpen}
+          mr="2"
+          display="flex"
+        >
+          
+        </IconButton>
+      )}
       <Logo />
 
       { isWideVersion && <SearchBox /> }
